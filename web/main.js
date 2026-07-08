f039a192530cc75b474445e236a95a06bea31543
prinzpuma <joshuagayer1@gmail.com>
2026-07-08 19:33:24 +0200

    Fix initialization timing by executing window bindings before loading local config

--- web/main.js
+++ web/main.js
@@ -1,12 +1,5 @@
 "use strict";
 
-// Optional config.local.js laden
-try {
-	await import("./config.local.js");
-} catch (e) {
-	console.log("Lokale Konfiguration (config.local.js) nicht geladen oder nicht vorhanden.");
-}
-
 import { U } from "./util.js";
 import { DB } from "./db.js";
 import { SRS } from "./srs.js";
@@ -67,3 +60,10 @@ window.openCardEditor = RENDER_ANKI.openCardEditor;
 window.seedIfEmpty = APP.seedIfEmpty;
 window.wireEvents = APP.wireEvents;
 window.purgeOldTrash = APP.purgeOldTrash;
+
+// Optional config.local.js laden
+try {
+	await import("./config.local.js");
+} catch (e) {
+	console.log("Lokale Konfiguration (config.local.js) nicht geladen oder nicht vorhanden.");
+}
