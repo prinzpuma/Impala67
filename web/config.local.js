fdd05b0d684c17956c42d64c0b90c6c866bebc01
prinzpuma <joshuagayer1@gmail.com>
2026-07-11 00:24:06 +0200

    config.local.js nicht mehr tracken

--- web/config.local.js
+++
@@ -1,6 +0,0 @@
-window.APP_CONFIG = {
-	GOOGLE_DESKTOP_CLIENT_ID: "100283147644-rpr91e1rbnllu66t86oahe876ms01m60.apps.googleusercontent.com",
-	// Google verlangt das Secret beim Desktop-Token-Tausch auch mit PKCE (Fehler „client_secret is missing“).
-	// Bei Desktop-Apps gilt es laut Google nicht als geheim. HIER DEIN (frisch rotiertes) SECRET EINTRAGEN:
-	GOOGLE_DESKTOP_CLIENT_SECRET: "GOCSPX-HIER_DEIN_SECRET"
-};
